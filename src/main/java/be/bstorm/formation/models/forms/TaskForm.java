3ab047281277a9ba46bf5d22759d659243d2b64e
YLorthPro <yann.lorthioir@bstorm.be>
2023-10-27 15:44:06 +0200

    Demo Annotation

--- src/main/java/be/bstorm/formation/models/forms/TaskForm.java
+++ src/main/java/be/bstorm/formation/models/forms/TaskForm.java
@@ -1,6 +1,8 @@
 package be.bstorm.formation.models.forms;
 
 import be.bstorm.formation.models.entities.Status;
+import be.bstorm.formation.validation.constraints.InFuture;
+import be.bstorm.formation.validation.constraints.NotEquals;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -8,15 +10,15 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Data
 public class TaskForm{
-    @FutureOrPresent
-    @NotNull(message = "Pas null coco")
     private LocalDate date;
     @NotBlank
     @Size(min = 4)
     private String name;
+    @NotEquals(message = "Pas coucou Gaetan", value = "bonjour")
     private String description;
     private Status status;
 }
---
+++ src/main/java/be/bstorm/formation/validation/validators/InFutureValidator.java
@@ -0,0 +1,54 @@
+package be.bstorm.formation.validation.validators;
+
+import be.bstorm.formation.validation.constraints.InFuture;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.Temporal;
+
+public class InFutureValidator implements ConstraintValidator<InFuture, Temporal> {
+
+    private InFuture annotation;
+
+    @Override
+    public void initialize(InFuture constraintAnnotation) {
+        annotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(Temporal value, ConstraintValidatorContext context) {
+
+        if(value == null)
+            return true;
+
+        context.disableDefaultConstraintViolation();
+
+        if(
+                (value instanceof LocalDate && !checkLocalDateValid((LocalDate)value) ) ||
+                (value instanceof LocalDateTime && !checkLocalDateTime((LocalDateTime)value) )
+        ){
+            context.buildConstraintViolationWithTemplate("Should be "+annotation.amount()+" "+annotation.unit().name().toLowerCase()+" in the future" )
+                    .addConstraintViolation();
+            return false;
+        }
+        else if( !(value instanceof LocalDate) && !(value instanceof LocalDateTime)  ) {
+            throw  new IllegalArgumentException();
+        }
+        else
+            return true;
+    }
+
+    private boolean checkLocalDateValid(LocalDate localDate){
+        return LocalDate.now()
+                .plus( annotation.amount(), annotation.unit() )
+                .isBefore( localDate );
+    }
+
+    private boolean checkLocalDateTime(LocalDateTime localDateTime){
+        return LocalDateTime.now()
+                .plus( annotation.amount(), annotation.unit() )
+                .isBefore( localDateTime );
+    }
+}
