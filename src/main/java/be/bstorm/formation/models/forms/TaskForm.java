d904539ccef0587b2f01fc5ce4ca1a2c1621d91a
YLorthPro <yann.lorthioir@bstorm.be>
2023-10-27 13:30:39 +0200

    init commit

--- src/main/java/be/bstorm/formation/models/forms/TaskForm.java
+++ src/main/java/be/bstorm/formation/models/forms/TaskForm.java
@@ -2,7 +2,9 @@ package be.bstorm.formation.models.forms;
 
 import be.bstorm.formation.models.entities.Status;
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,8 +12,10 @@ import java.time.LocalDate;
 @Data
 public class TaskForm{
     @FutureOrPresent
-    @NotNull
+    @NotNull(message = "Pas null coco")
     private LocalDate date;
+    @NotBlank
+    @Size(min = 4)
     private String name;
     private String description;
     private Status status;
