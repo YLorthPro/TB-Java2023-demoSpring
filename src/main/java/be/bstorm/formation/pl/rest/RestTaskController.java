8cc9cb0363b1b5fc97120e1b4ca3b9407a92dccd
YLorthPro <yann.lorthioir@bstorm.be>
2023-11-02 10:41:41 +0100

    Rest update

--- src/main/java/be/bstorm/formation/pl/rest/RestTaskController.java
+++ src/main/java/be/bstorm/formation/pl/rest/RestTaskController.java
@@ -2,11 +2,10 @@ package be.bstorm.formation.pl.rest;
 
 import be.bstorm.formation.bll.service.TaskService;
 import be.bstorm.formation.pl.mvc.models.dto.Task;
-import jakarta.persistence.SecondaryTable;
+import be.bstorm.formation.pl.mvc.models.forms.TaskForm;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -24,4 +23,10 @@ public class RestTaskController {
     public ResponseEntity<Set<Task>> getAll(){
         return ResponseEntity.ok(taskService.getAll("yann").stream().map(Task::toDTO).collect(Collectors.toSet()));
     }
+    
+    @PostMapping("/create")
+    public void create(@RequestBody @Valid TaskForm form){
+        taskService.create(form);
+    }
+    
 }
